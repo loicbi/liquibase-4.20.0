5ee5a63d229ecae7dc5c52a7eb145488bd7dd2cf
loicbi <loic.andre.bi@gmail.com>
2023-05-07 22:18:00 -0400

    firs sql 1.0

--- myChangeLog.sql
+++ myChangeLog.sql
@@ -16,3 +16,7 @@ create or replace table DEMO_DB.ALF_LIQUIBASE.QUALTRICS
 --comment: insert a
 INSERT INTO DEMO_DB.ALF_LIQUIBASE.QUALTRICS (SURVEY_ID, EXECUTION_FLAG)
 VALUES ('hhh', true);
+
+--changeset a.loic:3 labels:example-label
+INSERT INTO DEMO_DB.ALF_LIQUIBASE.QUALTRICS (SURVEY_ID, EXECUTION_FLAG)
+VALUES ('aaaa', true);
